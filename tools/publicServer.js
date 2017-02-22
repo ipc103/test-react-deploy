7863cc95d44e48132e06c53261640b414e10ab85
ipc103 <iancandysemail@gmail.com>
2017-02-22 14:00:04 -0500

    try again

--- tools/publicServer.js
+++ tools/publicServer.js
@@ -13,7 +13,7 @@ app.use(compression());
 app.use(express.static('build'));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.listen(port, function(err) {
