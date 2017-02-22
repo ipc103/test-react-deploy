70853aee4ea04894dfa521c9a6f6c6d1e86ea7b2
ipc103 <iancandysemail@gmail.com>
2017-02-22 14:10:31 -0500

    trying something

--- tools/publicServer.js
+++ tools/publicServer.js
@@ -15,7 +15,7 @@ app.use(express.static('build'));
 
 app.get('*', function(req, res) {
   console.log(__dirname)
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, function(err) {
